8b8746646fb004afb03bfa3665bf56881641364a
Jack <jack@lemonlime.tech>
2023-12-02 10:44:19 -0800

    day 2 does not work I guess

--- 2023/day1/src/lib.rs
+++ 2023/day1/src/lib.rs
@@ -1,9 +1,21 @@
-use std::io::{self, BufRead};
+
+use std::{io::{self, BufRead}, ops::Index, collections::{hash_map, HashMap}};
 use regex::Regex; // I wish I used regex instead. would be very clean.
 
 pub fn run() {
     let stdin = io::stdin();
-    part_one_and_two(&stdin, true);
+    let lines_in: Vec<String> = stdin.lock().lines().map( |v| { 
+        match v {
+            Ok(val) => {
+                val
+            },
+            Err(e) => {
+                println!("Error reading line from standard in! {}", e);
+                String::from("")
+            },
+        }
+    }).collect();
+    part_one_and_two(lines_in, true);
     //Entry point to the day's code
     
 
@@ -11,14 +23,30 @@ pub fn run() {
 }
 
 
-fn part_one_and_two(stdin: &io::Stdin, replace_text: bool) {
+//Mostly 2 now
+fn part_one_and_two(lines_in: Vec<String>, replace_text: bool) {
     //Part 1 of the day's challenge
 
     let mut sum: u32 = 0;
 
 
-    for line in stdin.lock().lines() {
-        let line: String = line.expect("Could not read line from standard in!");
+    //for part 2
+    let num_types: HashMap<String, String> = [
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ].iter().cloned().map( | (k, v) | { (k.to_string(), v.to_string()) }).collect();
+
+
+    for line in lines_in {
+
+        println!("Line: {}", line);
         
         //each line
 
@@ -27,20 +55,49 @@ fn part_one_and_two(stdin: &io::Stdin, replace_text: bool) {
 
         let replaced_text: String = match replace_text {
             true => {
-                line
-                .replace("one", "1")
-                .replace("two", "2")
-                .replace("three", "3")
-                .replace("four", "4")
-                .replace("five", "5")
-                .replace("six", "6")
-                .replace("seven", "7")
-                .replace("eight", "8")
-                .replace("nine", "9")
+
+
+
+                let mut line_vec: Vec<char> = line.clone().chars().collect();
+
+            
+                'outer: for i in 0..line_vec.len() {
+                    if i >= line_vec.len() || line_vec[i].is_digit(10) {
+                        continue;
+                    }
+
+                    'inner: for num_type in num_types.iter() {
+   
+                        let str_as_char: Vec<char> = num_type.0.chars().collect();
+            
+                        if line_vec[i..].starts_with(str_as_char.as_slice()) {
+                            for itr in i..(num_type.0.len() + i) {
+                                if itr < line_vec.len() {
+                                    line_vec[itr] = ' ';
+                                    
+                                }
+                            }
+                           
+        
+                            //Was the ' ' char.
+                            line_vec.remove(i);
+                            line_vec.insert(i, num_type.1.chars().last().unwrap());
+                            continue 'outer;
+                        }
+                    }
+
+                }
+
+              
+                line_vec.iter().collect::<String>()
+                
+
             },
             false => line.clone(),
         };
        
+
+
 
 
 
@@ -51,16 +108,38 @@ fn part_one_and_two(stdin: &io::Stdin, replace_text: bool) {
             }
         }
 
+        println!("Numbers: {:?}", nums);
+
         if nums.len() == 0 {
             println!("No numbers found in line!");
             continue;
         }
-        println!("nums: {:?}", nums);
-        println!("them added: {}", (nums[0] as u32) * 10 + (nums[nums.len() - 1] as u32));
-        sum += (nums[0] as u32) * 10 + (nums[nums.len() - 1] as u32);
+        let line_sum = (nums[0] as u32) * 10 + (nums[nums.len() - 1] as u32);
+        sum += line_sum;
+
+        println!("Line sum: {}", line_sum);
 
 
     }
     println!("Sum: {}", sum);
     
+}
+
+
+#[test]
+fn test_part_one_and_two() {
+    let vec_of = vec![
+        "one23nineight".to_string(),
+        "twothree46f".to_string(),
+        "threetwo4forme".to_string(),
+        "four".to_string(),
+        "five".to_string(),
+        "six".to_string(),
+        "sevenine".to_string(),
+        "eight".to_string(),
+        "eightnine".to_string(),
+        "nineight".to_string(),
+    ];
+    part_one_and_two(vec_of, true);
+    //assert_eq!(part_one_and_two(&stdin, false), 0);
 }
