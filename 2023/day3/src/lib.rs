40b48a89e1a8812d25982093fe0a7898554c1a33
Jack <jack@lemonlime.tech>
2023-12-03 19:57:09 -0800

    get ready for day 4

--- 2023/day3/src/lib.rs
+++ 2023/day3/src/lib.rs
@@ -1,15 +1,11 @@
 use common::get_input;
 
 pub fn run() {
-    
-    
     let input = get_input();
-
 
     part_one(&input);
     part_two(&input);
 }
-
 
 #[derive(Debug)]
 struct Position {
@@ -35,144 +31,152 @@ struct Symbol {
     symbol: char,
 }
 
-
 #[derive(Debug)]
 struct Schematic {
     map: Vec<EntryType>,
 }
 
 fn part_one(input: &Vec<String>) {
-
-
-
     let schematic = parse_input(input);
 
-
-    let symbols = schematic.map.iter().filter_map(|entry| {
-        match entry {
+    let symbols = schematic
+        .map
+        .iter()
+        .filter_map(|entry| match entry {
             EntryType::SYMBOL(symbol) => Some(symbol),
             _ => None,
-        }
-    }).collect::<Vec<&Symbol>>();
+        })
+        .collect::<Vec<&Symbol>>();
 
-
-    let parts = schematic.map.iter().filter_map(|entry| {
-        match entry {
+    let parts = schematic
+        .map
+        .iter()
+        .filter_map(|entry| match entry {
             EntryType::PART(part) => Some(part),
             _ => None,
-        }
-    }).collect::<Vec<&Part>>();
-
-
-    let touching_parts = parts.iter().filter( | part | {
-        symbols.iter().any(|symbol| {
-            is_part_touching_pos(part, &symbol.position)
         })
-    }).collect::<Vec<&&Part>>();
+        .collect::<Vec<&Part>>();
 
+    let touching_parts = parts
+        .iter()
+        .filter(|part| {
+            symbols
+                .iter()
+                .any(|symbol| is_part_touching_pos(part, &symbol.position))
+        })
+        .collect::<Vec<&&Part>>();
 
-    let sum: u64 = touching_parts.iter().fold(0, |acc, part| {
-        acc + part.value as u64
-    });
-
+    let sum: u64 = touching_parts
+        .iter()
+        .fold(0, |acc, part| acc + part.value as u64);
 
     println!("Part one: {}", sum);
 }
 
-
 fn part_two(input: &Vec<String>) {
     let schematic = parse_input(input);
 
-    
-
-    let symbols = schematic.map.iter().filter_map(|entry| {
-        match entry {
+    let symbols = schematic
+        .map
+        .iter()
+        .filter_map(|entry| match entry {
             EntryType::SYMBOL(symbol) => Some(symbol),
             _ => None,
-        }
-    }).collect::<Vec<&Symbol>>();
+        })
+        .collect::<Vec<&Symbol>>();
 
-    let parts = schematic.map.iter().filter_map(|entry| {
-        match entry {
+    let parts = schematic
+        .map
+        .iter()
+        .filter_map(|entry| match entry {
             EntryType::PART(part) => Some(part),
             _ => None,
-        }
-    }).collect::<Vec<&Part>>();
+        })
+        .collect::<Vec<&Part>>();
 
-    let symbols_touching_exactly_two_parts = symbols.iter().filter(| s | s.symbol == '*').filter_map(|symbol| {
-        let connected_part_values= parts.iter().filter_map(|part| {
-            if is_part_touching_pos(part, &symbol.position) {
-                return Some(part.value);
+    let symbols_touching_exactly_two_parts = symbols
+        .iter()
+        .filter(|s| s.symbol == '*')
+        .filter_map(|symbol| {
+            let connected_part_values = parts
+                .iter()
+                .filter_map(|part| {
+                    if is_part_touching_pos(part, &symbol.position) {
+                        return Some(part.value);
+                    } else {
+                        return None;
+                    }
+                })
+                .collect::<Vec<u32>>();
+            if connected_part_values.len() == 2 {
+                return Some((connected_part_values[0], connected_part_values[1]));
             } else {
                 return None;
             }
-        }).collect::<Vec<u32>>();
-        if connected_part_values.len() == 2 {
-            return Some((connected_part_values[0], connected_part_values[1]));
-        } else {
-            return None;
-        }
-    }).collect::<Vec<(u32, u32)>>();
+        })
+        .collect::<Vec<(u32, u32)>>();
 
-    let sum: u64 = symbols_touching_exactly_two_parts.iter().map(|values| {
-        values.0 as u64 * values.1 as u64
-    }).sum();
+    let sum: u64 = symbols_touching_exactly_two_parts
+        .iter()
+        .map(|values| values.0 as u64 * values.1 as u64)
+        .sum();
 
     println!("Part two: {:?}", sum);
 }
-
-
 
 fn parse_input(input: &Vec<String>) -> Schematic {
     let mut map: Vec<EntryType> = Vec::new();
 
     for (y, line) in input.iter().enumerate() {
-
         let mut skip = 0;
         for (x, c) in line.chars().enumerate() {
-
             if skip > 0 {
                 skip -= 1;
                 continue;
             }
 
             if let Some(_) = c.to_digit(10) {
+                let iter: Vec<char> = line
+                    .chars()
+                    .skip(x)
+                    .take_while(|c| c.is_digit(10))
+                    .collect();
 
-                let iter: Vec<char> = line.chars().skip(x)
-                .take_while(|c| c.is_digit(10))
-                .collect();
+                let positions: Vec<Position> = iter
+                    .iter()
+                    .enumerate()
+                    .map(|(i, _)| Position { x: x + i, y })
+                    .collect();
 
-
-                let positions: Vec<Position> = iter.iter().enumerate().map(|(i, _)| Position { x: x + i, y }).collect();
-
-                let folded_value_string = iter.iter().fold(String::new(), |mut acc, c| { acc.push(*c); acc });
-            
+                let folded_value_string = iter.iter().fold(String::new(), |mut acc, c| {
+                    acc.push(*c);
+                    acc
+                });
 
                 let parsed_value = match folded_value_string.parse::<u32>() {
-                    Ok(val) => {
-                        val
-                    },
+                    Ok(val) => val,
                     Err(err) => {
                         println!("Error: {}", err);
                         0
-                    },
+                    }
                 };
 
-
                 skip = positions.len().clone() - 1;
-                
 
-                map.push(EntryType::PART(Part { positions, value: parsed_value }));
+                map.push(EntryType::PART(Part {
+                    positions,
+                    value: parsed_value,
+                }));
 
                 //skip by positions.len()
-                
-
             } else if c != '.' {
-                map.push(EntryType::SYMBOL(Symbol { position: Position { x, y }, symbol: c }));
+                map.push(EntryType::SYMBOL(Symbol {
+                    position: Position { x, y },
+                    symbol: c,
+                }));
             } else {
                 //Don't worry about . they are suppose to be empty space.
             }
-
         }
     }
 
@@ -184,11 +188,11 @@ fn distance_between(a: usize, b: usize) -> u32 {
 }
 
 fn is_part_touching_pos(part: &Part, position: &Position) -> bool {
-    part.positions.iter().any(| part_pos| {
-        distance_between(part_pos.x, position.x) <= 1 && distance_between(position.y, part_pos.y) <= 1
+    part.positions.iter().any(|part_pos| {
+        distance_between(part_pos.x, position.x) <= 1
+            && distance_between(position.y, part_pos.y) <= 1
     })
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -202,8 +206,6 @@ mod tests {
             .collect::<Vec<String>>();
 
         let schematic = parse_input(&input);
-
-
 
         //rebuilt input
         let mut rebuilt_input = String::new();
@@ -219,14 +221,14 @@ mod tests {
                                 found = true;
                                 break;
                             }
-                        },
+                        }
                         EntryType::PART(part) => {
                             if part.positions.iter().any(|pos| pos.x == x && pos.y == y) {
                                 rebuilt_input.push_str(&part.value.to_string());
                                 found = true;
                                 break;
                             }
-                        },
+                        }
                     }
                 }
 
@@ -236,8 +238,6 @@ mod tests {
             }
             rebuilt_input.push('\n');
         }
-
-
 
         // println!("{:?}", &schematic)
     }
---
+++ 2023/day4/src/lib.rs
@@ -0,0 +1,23 @@
+use common::get_input;
+
+pub fn run() {
+    println!("Day 4");
+
+    let input = get_input();
+    part_one(input);
+}
+
+fn part_one(input: Vec<String>) {
+    //not yet!
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+
+        //Not yet!
+    }
+}
