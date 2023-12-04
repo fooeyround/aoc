772f8c7d4c260ac0be33686a25e69c9105190264
Jack <Jack@lemonlime.tech>
2023-12-04 10:13:37 -0800

    OFF BY ONE

--- 2023/day4/src/lib.rs
+++ 2023/day4/src/lib.rs
@@ -1,10 +1,13 @@
+use std::collections::HashMap;
+
 use common::get_input;
 
 pub fn run() {
     println!("Day 4");
 
     let input = get_input();
-    part_one(input);
+    part_one(&input);
+    part_two(&input);
 }
 
 
@@ -20,6 +23,22 @@ struct ProcessedCard {
     id: u32,
     winning_count : u32,
 }
+
+
+struct CardCounter {
+    /// First is the id, second is the ammount of said id. Use a list of `ProccessedCard`s as a LUT
+    map: HashMap<u32,u32>
+}
+
+impl CardCounter {
+    fn new() -> CardCounter {
+        CardCounter {
+            map: HashMap::new()
+        }
+    }
+}
+
+
 impl ProcessedCard {
     fn from_card(card: &Card) -> ProcessedCard {
         let winning_count = card.given_numbers.iter().filter(| num | card.wining_numbers.contains(num)).count() as u32;
@@ -30,7 +49,7 @@ impl ProcessedCard {
     }
 }
 
-fn parse_input(input: Vec<String>) -> Vec<Card> {
+fn parse_input(input: &Vec<String>) -> Vec<Card> {
 
     let parsed = input.iter().map(| line | {
         let game_split = line.split(":").collect::<Vec<&str>>();
@@ -56,7 +75,7 @@ fn parse_input(input: Vec<String>) -> Vec<Card> {
    parsed
 }
 
-fn part_one(input: Vec<String>) {
+fn part_one(input: &Vec<String>) {
     //not yet!
 
 
@@ -73,7 +92,7 @@ fn part_one(input: Vec<String>) {
 
 }
 
-fn part_two(input: Vec<String>) {
+fn part_two(input: &Vec<String>) {
    
    let proccessed_cards = parse_input(input).iter().map(| card | ProcessedCard::from_card(card)).collect::<Vec<ProcessedCard>>();
 
@@ -92,10 +111,56 @@ fn part_two(input: Vec<String>) {
         for _ in 0..clones {
             with_cloned_cards.push(card.clone());
         }
-        println!("Card {} has {} clones", card.id, clones);
     }
 
 
+
+    let original_cards = proccessed_cards.clone();
+    let mut card_counter= CardCounter {
+        map: original_cards.iter().fold(HashMap::new(), |acc, u| {
+            //I know cloning it ever time is horrible... Too lazy though.
+            let mut new_cloned = acc.clone();
+            new_cloned.insert(u.id, u.winning_count);
+            new_cloned
+        })
+    };
+
+
+    let mut current_card_id = 1;
+    loop {
+
+        if card_counter.map.len()-1 < current_card_id {
+            break;
+        }
+
+        let current_card_win_count = card_counter.map.get(&(current_card_id as u32)).unwrap_or(&0);
+
+        for i in current_card_id..(current_card_win_count.clone() as usize + current_card_id) {
+            let before_value = card_counter.map.get(&(i as u32)).unwrap_or(&0);
+            card_counter.map.insert(i as u32, before_value + 1);
+
+        }
+
+
+
+
+        current_card_id += 1;
+
+        println!("card_id: {}, car_counter_map_len: {}", current_card_id, card_counter.map.len());
+
+        //do for current card id
+
+
+    }
+
+ 
+    println!("{:?}", card_counter.map);
+
+    let sum = card_counter.map.iter().fold(0, |acc, num| { acc + num.1});
+
+
+
+    println!("Part 2: {}", sum);
 
 
 }
@@ -110,8 +175,21 @@ mod tests {
         let input = include_str!("test_input.txt").split("\n").map(| line | line.to_string()).collect::<Vec<String>>();
 
         
-        part_two(input);
+        part_one(&input);
 
         //Not yet!
     }
+
+
+    #[test]
+    fn test_part_two() {
+
+        let input = include_str!("test_input.txt").split("\n").map(| line | line.to_string()).collect::<Vec<String>>();
+
+        
+        part_two(&input);
+
+        //Not yet!
+    }
+
 }
