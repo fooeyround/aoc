dd800f16cf826bc4ee74313b01c1d934a37a2f11
Jack <jack@lemonlime.tech>
2023-12-04 20:00:32 -0800

    more stuff

--- 2023/day4/src/lib.rs
+++ 2023/day4/src/lib.rs
@@ -10,7 +10,6 @@ pub fn run() {
     part_two(&input);
 }
 
-
 #[derive(Debug)]
 struct Card {
     id: u32,
@@ -21,27 +20,29 @@ struct Card {
 #[derive(Clone)]
 struct ProcessedCard {
     id: u32,
-    winning_count : u32,
+    winning_count: u32,
 }
-
 
 struct CardCounter {
     /// <id, count>
-    map: HashMap<u32,u32>
+    map: HashMap<u32, u32>,
 }
 
 impl CardCounter {
     fn new() -> CardCounter {
         CardCounter {
-            map: HashMap::new()
+            map: HashMap::new(),
         }
     }
 }
 
-
 impl ProcessedCard {
     fn from_card(card: &Card) -> ProcessedCard {
-        let winning_count = card.given_numbers.iter().filter(| num | card.wining_numbers.contains(num)).count() as u32;
+        let winning_count = card
+            .given_numbers
+            .iter()
+            .filter(|num| card.wining_numbers.contains(num))
+            .count() as u32;
         ProcessedCard {
             id: card.id,
             winning_count: winning_count,
@@ -50,107 +51,101 @@ impl ProcessedCard {
 }
 
 fn parse_input(input: &Vec<String>) -> Vec<Card> {
+    let parsed = input
+        .iter()
+        .map(|line| {
+            let game_split = line.split(":").collect::<Vec<&str>>();
 
-    let parsed = input.iter().map(| line | {
-        let game_split = line.split(":").collect::<Vec<&str>>();
+            let game_id = game_split[0].split_whitespace().collect::<Vec<&str>>()[1]
+                .parse::<u32>()
+                .unwrap();
 
-        let game_id = game_split[0].split_whitespace().collect::<Vec<&str>>()[1].parse::<u32>().unwrap();
+            let both_numbers_vec = game_split[1].split("|").collect::<Vec<&str>>();
 
-        let both_numbers_vec = game_split[1].split("|").collect::<Vec<&str>>();
+            let wining_numbers = both_numbers_vec[0]
+                .split_whitespace()
+                .collect::<Vec<&str>>()
+                .iter()
+                .map(|num| num.parse::<u32>().unwrap())
+                .collect::<Vec<u32>>();
 
-        
+            let given_numbers_raw = both_numbers_vec[1]
+                .split_whitespace()
+                .collect::<Vec<&str>>()
+                .iter()
+                .map(|num| num.parse::<u32>().unwrap())
+                .collect::<Vec<u32>>();
 
-        let wining_numbers = both_numbers_vec[0].split_whitespace().collect::<Vec<&str>>().iter().map(| num | num.parse::<u32>().unwrap()).collect::<Vec<u32>>();
+            Card {
+                id: game_id,
+                wining_numbers: wining_numbers,
+                given_numbers: given_numbers_raw,
+            }
+        })
+        .collect::<Vec<Card>>();
 
-        let given_numbers_raw = both_numbers_vec[1].split_whitespace().collect::<Vec<&str>>().iter().map(| num | num.parse::<u32>().unwrap()).collect::<Vec<u32>>();
-
-        Card {
-            id: game_id,
-            wining_numbers: wining_numbers,
-            given_numbers: given_numbers_raw,
-        }
-        
-    }).collect::<Vec<Card>>();
-
-   parsed
+    parsed
 }
 
 fn part_one(input: &Vec<String>) {
     //not yet!
 
-
     let cards = parse_input(input);
 
-
-    let result: u32 = cards.iter().map( | card | {
-        card.given_numbers.iter().filter(| num | card.wining_numbers.contains(num)).fold(0, | acc, _ | if acc == 0 { 1} else { acc * 2 })
-    }).sum();
-
-
+    let result: u32 = cards
+        .iter()
+        .map(|card| {
+            card.given_numbers
+                .iter()
+                .filter(|num| card.wining_numbers.contains(num))
+                .fold(0, |acc, _| if acc == 0 { 1 } else { acc * 2 })
+        })
+        .sum();
 
     println!("Part 1: {}", result);
-
 }
 
-
-
 fn part_two(input: &Vec<String>) {
-   
-   let proccessed_cards = parse_input(input).iter().map(| card | ProcessedCard::from_card(card)).collect::<Vec<ProcessedCard>>();
-
-
-
-
+    let proccessed_cards = parse_input(input)
+        .iter()
+        .map(|card| ProcessedCard::from_card(card))
+        .collect::<Vec<ProcessedCard>>();
 
     let mut winning_map: HashMap<u32, u32> = HashMap::new();
-    proccessed_cards.iter().for_each(| card | {
+    proccessed_cards.iter().for_each(|card| {
         winning_map.insert(card.id, card.winning_count);
     });
 
-
-
-    
     //id to count
     let mut card_counter: HashMap<u32, u32> = HashMap::new();
 
-
     //init the card counter with starting cards. You only start with one of each, even if you win multiple times. there would be multiple card instances.
-    proccessed_cards.iter().for_each(| card | {
+    proccessed_cards.iter().for_each(|card| {
         //deref here does not take form the map right? it passes by value.
         card_counter.insert(card.id, 1);
     });
 
-
     let mut current_card_id = 0;
     while card_counter.len() > current_card_id {
         current_card_id += 1;
-    
-
-
 
         let card_win_count = winning_map.get(&(current_card_id as u32)).unwrap_or(&0);
 
-        let card_count = card_counter.get(&(current_card_id as u32)).unwrap_or(&0).clone();
+        let card_count = card_counter
+            .get(&(current_card_id as u32))
+            .unwrap_or(&0)
+            .clone();
 
-
-        for i in (current_card_id+1)..(card_win_count.clone() as usize + current_card_id+1) {
+        for i in (current_card_id + 1)..(card_win_count.clone() as usize + current_card_id + 1) {
             let before_value = card_counter.get(&(i as u32)).unwrap_or(&0);
             card_counter.insert(i as u32, before_value + card_count);
-            
-
         }
     }
 
-    let sum = card_counter.iter().fold(0, |acc, num| { acc + num.1});
-
-
+    let sum = card_counter.iter().fold(0, |acc, num| acc + num.1);
 
     println!("Part 2: {}", sum);
-
-
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -158,25 +153,25 @@ mod tests {
 
     #[test]
     fn test_part_one() {
+        let input = include_str!("test_input.txt")
+            .split("\n")
+            .map(|line| line.to_string())
+            .collect::<Vec<String>>();
 
-        let input = include_str!("test_input.txt").split("\n").map(| line | line.to_string()).collect::<Vec<String>>();
-
-        
         part_one(&input);
 
         //Not yet!
     }
 
-
     #[test]
     fn test_part_two() {
+        let input = include_str!("test_input.txt")
+            .split("\n")
+            .map(|line| line.to_string())
+            .collect::<Vec<String>>();
 
-        let input = include_str!("test_input.txt").split("\n").map(| line | line.to_string()).collect::<Vec<String>>();
-
-        
         part_two(&input);
 
         //Not yet!
     }
-
 }
