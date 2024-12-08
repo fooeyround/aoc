7665983ee73e2846b6edfeed97c6b5845a64561f
fooeyround <DevJackMC@gmail.com>
2024-12-08 03:48:40 -0800

    move to iterator style loops for day one

--- 2024/src/d1.rs
+++ 2024/src/d1.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 pub fn solve1(raw_input: &str) -> String {
     let input: Vec<String> = {
         raw_input
@@ -7,22 +9,23 @@ pub fn solve1(raw_input: &str) -> String {
             .collect()
     };
 
-    let mut lists = (Vec::new(), Vec::new());
+    let (mut a, mut b): (Vec<u32>, Vec<u32>) = input
+        .iter()
+        .filter_map(|line| line.trim().split_once(" "))
+        .map(|(a, b)| {
+            (
+                a.trim().parse::<u32>().expect("Number"),
+                b.trim().parse::<u32>().expect("Number"),
+            )
+        })
+        .collect();
 
-    for line in input {
-        let Some(bb) = line.trim().split_once(" ") else {
-            continue;
-        };
-        lists.0.push(bb.0.trim().parse::<u32>().expect("Number"));
-        lists.1.push(bb.1.trim().parse::<u32>().expect("Number"));
-    }
-
-    lists.0.sort();
-    lists.1.sort();
+    a.sort();
+    b.sort();
 
     let mut count = 0;
-    for i in lists.0.iter().zip(lists.1.iter()) {
-        count += (i.0).abs_diff(*i.1);
+    for (i, j) in a.iter().zip(b.iter()) {
+        count += (i).abs_diff(*j);
     }
     return count.to_string();
 }
