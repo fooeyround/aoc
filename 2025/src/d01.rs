cbe057912fa997c754e36901809b9d8853063810
fooeyround <75762996+fooeyround@users.noreply.github.com>
2025-12-07 13:03:14 -0800

    start 2025

---
+++ 2025/src/d01.rs
@@ -0,0 +1,105 @@
+use std::{os::unix::raw, panic};
+
+pub fn p1(input: &str) -> String {
+    let mut count = 0;
+    let mut rot = 50;
+
+    for line in input.split("\n").filter(|l| !l.trim().is_empty()) {
+        let left = line.starts_with('L');
+        let mult = if left { -1 } else { 1 };
+        let rel_rot = i32::from_str_radix(&line[1..], 10).expect("int") * mult;
+        rot = (rot + rel_rot).rem_euclid(100);
+        if rot == 0 {
+            count += 1;
+        }
+    }
+
+    return count.to_string();
+}
+pub fn p2(input: &str) -> String {
+    let mut count = 0;
+    let mut rot = 50;
+
+    for line in input.split("\n").filter(|l| !l.trim().is_empty()) {
+        let left = line.starts_with('L');
+        let mult = if left { -1 } else { 1 };
+        let rel = i32::from_str_radix(&line[1..], 10).expect("int") * mult;
+
+        //We are currently over-counting by 3 for my input...
+        let crosses = (rot + rel).div_euclid(100);
+        let new_rot = (rot + rel).rem_euclid(100);
+
+        count += crosses.abs();
+        if rot < 0 || rot >= 100 {
+            panic!();
+        }
+        //Correct for left roll over from 0.
+        if rot == 0 && new_rot != 0 && left && rel > -100 && rel < 0 {
+            count -= 1;
+            println!("FIX NEEDED?");
+        }
+        rot = new_rot;
+        println!("{} | {}", rot, count);
+    }
+
+    println!("END OF P1");
+    println!("REAL: {}", p2_(&input));
+    return count.to_string();
+}
+
+pub fn p2_(body: &str) -> String {
+    let start = 50;
+    let mut pos = start;
+    let mut count: i32 = 0;
+    let mut i = 0;
+
+    for movement in body.lines() {
+        i = i + 1;
+        let direction = movement.starts_with("L");
+        let mut movement_count: i32 = i32::from_str_radix(&movement[1..], 10).expect("int");
+        count += movement_count / 100;
+        movement_count = movement_count % 100;
+        match direction {
+            true => {
+                let new_pos = pos - movement_count;
+                let corrected_pos = match new_pos {
+                    x if x < 0 => {
+                        if pos > 0 {
+                            // println!("Went through zero, added 1 to the password");
+                            count = count + 1;
+                        };
+                        100 + new_pos
+                    }
+                    x if x >= 0 => new_pos,
+                    _ => panic!(),
+                };
+                pos = corrected_pos;
+            }
+            false => {
+                let new_pos = pos + movement_count;
+                let corrected_pos = match new_pos {
+                    x if x <= 99 => new_pos,
+                    x if x > 100 => {
+                        // println!("Went through zero, added 1 to the password");
+                        count = count + 1;
+                        new_pos - 100
+                    }
+                    100 => 0,
+                    _ => panic!(),
+                };
+                pos = corrected_pos;
+            }
+            _ => panic!(),
+        };
+        match pos {
+            0 => count = count + 1,
+            _ => (),
+        };
+        // println!(
+        //     "{}. Moved {} {}, ended up at {}",
+        //     i, direction, movement_count, pos
+        // );
+        // println!("{} | {}", pos, count)
+    }
+    return count.to_string();
+}
---
+++ 2025/src/main.rs
@@ -0,0 +1,157 @@
+// #[global_allocator]
+// static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
+use chrono::Datelike;
+use clap::Parser;
+use cli_table::{Table, WithTitle, print_stdout};
+use seq_macro::seq;
+use std::time::{Duration, Instant};
+use std::{fs::File, io::Read};
+
+pub type PartFn = fn(&str) -> String;
+
+pub struct Day {
+    pub day: u8,
+    pub part1: PartFn,
+    pub part2: PartFn,
+}
+
+inventory::collect!(Day);
+
+seq!(N in 01..=01 {
+    mod d~N;
+    inventory::submit! {
+        Day {
+            day: N,
+            part1: d~N::p1,
+            part2: d~N::p2,
+        }
+    }
+});
+
+#[derive(Table)]
+struct DayInfo {
+    #[table(title = "Day & Part")]
+    day_and_part: String,
+    #[table(title = "Time")]
+    time: String,
+    #[table(title = "Output")]
+    output: String,
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    day: Option<u8>,
+    #[arg(short, long, action)]
+    part_two_only: bool,
+    ///Enable test mode (Using test input instead of real input)
+    #[arg(short, long, action)]
+    test_mode: bool,
+}
+
+/// returns (Example, Real)
+pub fn get_input(day: u8, test_mode: bool) -> String {
+    let mut input = String::new();
+
+    let mut file = File::open(format!(
+        "puzzle_input/d{:02}.{}",
+        day,
+        if test_mode { "ex" } else { "in" }
+    ))
+    .expect("Day input missing");
+    file.read_to_string(&mut input)
+        .expect("Failed to read puzzle input for day");
+    return input;
+}
+
+pub fn get_display_time(duration: Duration) -> String {
+    if duration.as_secs() >= 1 {
+        return format!("{}secs", duration.as_secs());
+    }
+    if duration.as_millis() >= 1 {
+        return format!("{}ms", duration.as_millis());
+    }
+    if duration.as_micros() >= 1 {
+        return format!("{}μs", duration.as_micros());
+    }
+    return format!("{}ns", duration.as_nanos());
+}
+
+/*
+pub fn run_day(day: u8, day_impl: &impl Day, part_two_only: bool) {
+    let input = get_input(day);
+    if part_two_only {
+        let initial_time = Instant::now();
+        let output = (*day_impl).p1(&input);
+        println!(
+            "🎄Part 1 ({}): {}",
+            get_display_time(initial_time.elapsed()),
+            output
+        );
+    }
+    let initial_time = Instant::now();
+    let output = (*day_impl).p2(&input);
+    println!(
+        "🎄Part 2 ({}): {}",
+        get_display_time(initial_time.elapsed()),
+        output
+    );
+}
+*/
+
+fn run_timed<T>(func: impl Fn() -> T) -> (Duration, T) {
+    let initial_time = Instant::now();
+    let output = func();
+    return (initial_time.elapsed(), output);
+}
+
+fn main() {
+    println!("🎄Advent of Code 2025!🎄");
+
+    let cli = Cli::parse();
+
+    if cli.part_two_only {
+        println!("Running Only Part Two!");
+    }
+
+    let day_impls = inventory::iter::<Day>.into_iter().filter(|d| {
+        if let Some(day) = cli.day {
+            day == d.day
+        } else {
+            true
+        }
+    });
+    let mut data: Vec<DayInfo> = Vec::new();
+
+    for day_impl in day_impls {
+        let input = get_input(day_impl.day, cli.test_mode);
+        println!("🎄Testing Day {}", day_impl.day);
+        if !cli.part_two_only {
+            let (time, output) = run_timed(|| (day_impl.part1)(&input));
+            data.push(DayInfo {
+                day_and_part: format!("{:02} p1", day_impl.day),
+                time: get_display_time(time),
+                output: output,
+            });
+        }
+        let (time, output) = run_timed(|| (day_impl.part2)(&input));
+        data.push(DayInfo {
+            day_and_part: format!("{:02} p2", day_impl.day),
+            time: get_display_time(time),
+            output: output,
+        });
+    }
+
+    if cli.test_mode {
+        println!("🧪Test Mode Results");
+    } else {
+        println!("🎄Results");
+    }
+    print_stdout(data.with_title()).expect("Printable results");
+
+    // println!("🎄Part 2");
+    // println!("🎄Test ({}): {}", get_display_time(test_dir), test_output);
+    // println!("🎄Real ({}): {}", get_display_time(real_dir), real_output);
+}
